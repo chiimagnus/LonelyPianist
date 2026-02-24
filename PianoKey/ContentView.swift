e5a906340b3de787df4b297492fb142fcfca7f59
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-02-24 23:37:30 +0800

    Initial Commit

---
+++ PianoKey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PianoKey
+//
+//  Created by chii_magnus on 2026/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PianoKey/PianoKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoKeyApp.swift
+//  PianoKey
+//
+//  Created by chii_magnus on 2026/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PianoKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
