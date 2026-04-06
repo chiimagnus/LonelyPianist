81307d9cb77aebbca51c10dfa4b22cf352248c82
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-04-06 22:50:04 +0800

    init of avp app

---
+++ LonelyPianistAVP/LonelyPianistAVPApp.swift
@@ -0,0 +1,34 @@
+//
+//  LonelyPianistAVPApp.swift
+//  LonelyPianistAVP
+//
+//  Created by chii_magnus on 2026/4/6.
+//
+
+import SwiftUI
+
+@main
+struct LonelyPianistAVPApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
