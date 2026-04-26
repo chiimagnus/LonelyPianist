9d91dd9007f9745a5e070df1686355201d3e9704
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-04-26 17:09:30 +0800

    refactor(debug): 移除手部追踪调试控制器及相关颜色定义

--- LonelyPianistAVP/Services/RealityKit/AVPOverlayPalette.swift
+++ LonelyPianistAVP/Services/RealityKit/AVPOverlayPalette.swift
@@ -7,9 +7,6 @@ enum AVPOverlayPalette {
     static let a0AnchorColor: UIColor = .systemBlue.withAlphaComponent(0.9)
     static let c8AnchorColor: UIColor = .systemPurple.withAlphaComponent(0.9)
 
-    /// Hand tracking debug
-    static let handTipColor: UIColor = .systemGreen.withAlphaComponent(0.9)
-
     // Practice feedback
     static let feedbackNoneColor: UIColor = .init(red: 1.00, green: 0.93, blue: 0.78, alpha: 1.0)
     static let feedbackCorrectColor: UIColor = .init(red: 1.00, green: 0.95, blue: 0.82, alpha: 1.0)
